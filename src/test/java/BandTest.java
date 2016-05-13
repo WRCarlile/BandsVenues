2f8621db6e1ef2faa03e94893ac5ba0a19227ef1
Ryan Carlile <ryancarlile@gmail.com>
2016-05-13 11:30:58 -0700

    Write specs for saving and finding bands with successful builds

--- src/test/java/BandTest.java
+++ src/test/java/BandTest.java
@@ -34,57 +34,57 @@ public class BandTest {
     assertTrue(firstBand.equals(secondBand));
   }
 
-//   @Test
-//   public void save_savesObjectIntoDatabase_true() {
-//     Band myBand = new Band("The Beatles", "Rock");
-//     myBand.save();
-//     assertTrue(Band.all().get(0).equals(myBand));
-//   }
-//
-//   @Test
-//   public void save_assignsIdToObject_int() {
-//     Band myBand = new Band("The Beatles", "Rock");
-//     myBand.save();
-//     Band savedBand = Band.all().get(0);
-//     assertEquals(myBand.getId(), savedBand.getId());
-//   }
-//
-//   @Test
-//   public void find_findsBandInDatabase_true() {
-//     Band myBand = new Band("The Beatles", "Rock");
-//     myBand.save();
-//     Band savedBand = Band.find(myBand.getId());
-//     assertTrue(myBand.equals(savedBand));
-//   }
-//
-//   @Test
-//   public void update_updatesBandBandName_true() {
-//     Band myBand = new Band("The Beatles", "Rock");
-//     myBand.save();
-//     myBand.update("Take a nap");
-//     assertEquals("Take a nap", Band.find(myBand.getId()).getBandName());
-//   }
-//
-//   @Test
-//   public void delete_deletesBand_true() {
-//     Band myBand = new Band("The Beatles", "Rock");
-//     myBand.save();
-//     int myBandId = myBand.getId();
-//     myBand.delete();
-//     assertEquals(null, Band.find(myBandId));
-//   }
-//
-//   @Test
-//   public void addVenue_addsVenueToBand() {
-//     Venue myVenue = new Venue("Household chores");
-//     myVenue.save();
-//     Band myBand = new Band("The Beatles", "Rock");
-//     myBand.save();
-//     myBand.addVenue(myVenue);
-//     Venue savedVenue = myBand.getVenues().get(0);
-//     assertTrue(myVenue.equals(savedVenue));
-//   }
-//
+  @Test
+  public void save_savesObjectIntoDatabase_true() {
+    Band myBand = new Band("The Beatles", "Rock");
+    myBand.save();
+    assertTrue(Band.all().get(0).equals(myBand));
+  }
+
+  @Test
+  public void save_assignsIdToObject_int() {
+    Band myBand = new Band("The Beatles", "Rock");
+    myBand.save();
+    Band savedBand = Band.all().get(0);
+    assertEquals(myBand.getId(), savedBand.getId());
+  }
+
+  @Test
+  public void find_findsBandInDatabase_true() {
+    Band myBand = new Band("The Beatles", "Rock");
+    myBand.save();
+    Band savedBand = Band.find(myBand.getId());
+    assertTrue(myBand.equals(savedBand));
+  }
+
+  @Test
+  public void update_updatesBandBandName_true() {
+    Band myBand = new Band("The Beatles", "Rock");
+    myBand.save();
+    myBand.update("The Rolling Stones");
+    assertEquals("The Rolling Stones", Band.find(myBand.getId()).getBandName());
+  }
+
+  @Test
+  public void delete_deletesBand_true() {
+    Band myBand = new Band("The Beatles", "Rock");
+    myBand.save();
+    int myBandId = myBand.getId();
+    myBand.delete();
+    assertEquals(null, Band.find(myBandId));
+  }
+
+  @Test
+  public void addVenue_addsVenueToBand() {
+    Venue myVenue = new Venue("CBGBs", "555 Elm");
+    myVenue.save();
+    Band myBand = new Band("The Beatles", "Rock");
+    myBand.save();
+    myBand.addVenue(myVenue);
+    Venue savedVenue = myBand.getVenues().get(0);
+    assertTrue(myVenue.equals(savedVenue));
+  }
+
 //   @Test
 //   public void getVenues_returnsAllVenues_List() {
 //     Venue myVenue = new Venue("Household chores");
